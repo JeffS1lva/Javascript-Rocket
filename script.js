5cce046a63fe68f5f5f1ffcecf0fcafafd5474ce
JeffS1lva <118827532+JeffS1lva@users.noreply.github.com>
2023-09-28 09:31:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+console.log('Hello World')
+
+let numberOne = 20
+let numberTwo = 30
+console.log(numberOne + numberTwo)
+
+num = 12
+if(typeof num === 'number'){
+  console.log('é')
+} else{
+  console.log('não')
+}
+
+text = '123'
+if(typeof text === 'string'){
+  console.log('é')
+} else{
+  console.log('não')
+}
+
+boole = true
+if(typeof boole === 'boolean'){
+  console.log('é')
+} else{
+  console.log('não')
+}
+
+
+let numbOne = 4
+let numbTwo = 10
+console.log(numbOne - numbTwo)
+
+let MultOne = 4
+let MultTwo = 10
+console.log(numbOne * numbTwo)
+
+let divOne = 4
+let divTwo = 10
+console.log(numbOne / numbTwo)
+
+let par = 2
+if(par % 2 === 0){
+  console.log('Este número é par')
+} else {
+  console.log('Este número não é par')
+}
+
+let impar = 3
+if(impar %3 ===0){
+console.log('É um número impar')
+} else {
+  console.log('Não é um número ímpar ')
+}
+
+
+
+
+
+
+
+
+
+
